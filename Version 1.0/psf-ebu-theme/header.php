d038d86c8d328c71912219f4cf48909e2ea4c0c8
Parker-Smith-And-Feek <86122048+Parker-Smith-And-Feek@users.noreply.github.com>
2023-01-09 08:56:06 -0800

    Replacing db call
    Replacing DB call with $wpdb & prepare(). Previous functions needed extra permissions to work and I wanted to use built-in WP functions
    
    This is a functioning call to the DB and populating variables that will be used with the JS functions to populate containers.

--- Version 1.0/psf-ebu-theme/header.php
+++ Version 1.0/psf-ebu-theme/header.php
@@ -2,7 +2,38 @@
 <html class ='' lang=''>
     <!--Start head content-->
     <head>
-        <?php wp_head() ?>
+        <!--Get values from respective DB-tables. Will use this data to populate fields via JS/jQuery, to have more flexibility with future vs past posts-->
+        <?php
+        //Get values from respective DB-tables. Will use this data to populate fields via JS/jQuery, to have more flexibility with future vs past posts
+            global $wpdb;
+
+            $web_sql = $wpdb->prepare("SELECT *
+                FROM benefits_webinars
+                ORDER BY Date DESC", array("ID", 'Title', 'Date', 'Content', 'Handout', 'Presentation', 'Recording', 'Host')
+                );
+            $webinars_data = $wpdb->get_results($web_sql);
+
+            $art_sql = $wpdb->prepare("SELECT *
+                FROM articles
+                ORDER BY Date DESC", array("id", 'title', 'date', 'link', 'tags')
+                );
+            $benefit_alerts = $wpdb->get_results($art_sql);
+
+            $eb_sql = $wpdb->prepare("SELECT *
+                FROM employee_benefits
+                ORDER BY Date DESC", array("id", 'title', 'date', 'link', 'tags')
+                );
+            $employee_benefits = $wpdb->get_results($eb_sql);
+
+  ?>
+  <script>
+    let benefitsWebinars = <?php echo json_encode($webinars_data)?>;
+    console.log('benefitsWebinars: ', benefitsWebinars);
+    let benefitAlerts = <?php echo json_encode($benefit_alerts)?>;
+    let employeeBenefits = <?php echo json_encode($employee_benefits)?>;
+
+  </script>
+  <?php wp_head() ?>
     </head>
     <!--Start of Body content-->
     <body>
